177183b4704443bf017fbb2f03ef9a8d19c0b631
Prashant Singh <92790626+Prashant-Singh79@users.noreply.github.com>
2021-12-09 19:48:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [data, updateData] = useState("Marco");
+  const [btn, updateButton] = useState("Polo");
+  function change(e) {
+    if (e.target.id === "marco-polo-toggler") {
+      updateData(btn);
+      updateButton(data);
+    }
+  }
+  return (
+    <div className="App">
+      <h1 id="marco-polo">{data}</h1>
+      <button id="marco-polo-toggler" onClick={change}>
+        {btn}
+      </button>
+    </div>
+  );
+}
